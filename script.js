cdad92ce50cf2470b0a28cba4b9ab68df72b406d
stefansonntag <stefan@sonntag-online.com>
2015-07-30 11:12:16 +0200

    0.1
    Completely refactored, displays naked list

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-define( [ 	// Note: If you load .js files, omit the file extension, otherwise
-        	// requireJS will not load it correctly !
-			'jquery' ,
-			'./properties',
-			'text!./scoped-bootstrap.css',
-			'./bootstrap.min',
-			'text!./bootstrap-select.min.css',
-			'./bootstrap-select.min',
-			'qlik'
-		],
-
+define(
+	[ 	// Note: If you load .js files, omit the file extension, otherwise
+        // requireJS will not load it correctly !
+		'jquery' ,
+		'./properties',
+		'text!./scoped-bootstrap.css',
+		'./bootstrap.min',
+		'text!./bootstrap-select.min.css',
+		'./bootstrap-select.min',
+		'qlik'
+	],
     	function ($, properties, cssBootstrap, jsBootstrap, cssBootstrapSelect, jsBootstrapSelect, qlik) { 
     		// include dependency arguments, give them shortnames in the sequence listed under define[]  
     
         	'use strict';
 
+        	var maxListEntries = 5;
+
         	// use the Mashup API for building a list instead of a hypercube which seems to be the default in the extension API
         	var $currApp = qlik.currApp( this );
 
-
         	// define various functions that we will use in the return function
-        	var writeToConsole = function(){
+        	var writeToConsole = function() {
 				for(var i=0; i<arguments.length; i++) {
 					console.log( 'Argument [' + i + '] --> ');
 					console.log( arguments[i] );
 				};
 			}
 
-			var addCSS = function(CSS){
+			var addCSS = function(CSS) {
 
 					$( "<style>" ).html( CSS ).appendTo( "head" );
 			}
 
-			var addBootstrapDiv = function($element){
+			var addBootstrapDiv = function($element) {
 				var $scopedBootstrap = $( document.createElement( 'div' ) ); 
 				$scopedBootstrap.attr( 'class' , 'bootstrap_inside' );
 				$element.append( $scopedBootstrap );
 			}
 
-			var buildDropdown = function(field, top,left,heigth,width, command){
+			var buildDropdown = function(field, top,left,heigth,width, command) {
 				// first get the data, then build the HTML syntax for it, but don't insert it into the DOM yet.
 				// The reason is that the selectpicker library changes the select tag into a button dropdown statement and it
 				// becomes difficult to find the right entrypoints for the select option. So we build the entire string 1st first
@@ -67,43 +68,44 @@ define( [ 	// Note: If you load .js files, omit the file extension, otherwise
 				// build the HTML <SELECT> skeleton 
 				var $selectTag = '<select id="mySelect" class="selectpicker" ';
 				// see all selectpicker options at http://silviomoreto.github.io/bootstrap-select
-				var $selectTagOptions = 'multiple data-size="10" data-live-search="true" data-style="btn-inverse" header="true">';
+				var $selectTagOptions = 'multiple data-size="auto" data-live-search="false" data-style="btn-inverse">';
 				// start with "something", allows to better debug what gets added to the options string
-				var $optionList = "<option>Something</option>"; 
+				var $optionList = ""; 
 
 				switch(command){
-					case 'create':
+					
+					case 'create': {
 						$.each(reply.qListObject.qDataPages[0].qMatrix, function(key, value) {
 							$optionList += '<option>' + value[0].qText + '</option>' ;
-						});	 
-					default:
+						});
+						break;
+					};	 
+
+					default: {
 						$.each(reply.qListObject.qDataPages[0].qMatrix, function(key, value) {
 							$optionList += '<option>' + value[0].qText + '</option>' ;
 						}); 
+					};
 				}
 
 				// add the entire <SELECT> plus <OPTION>'s' dropdown to the DOM inside the boostrapped div's
 				$( '.bootstrap_inside' ).append( $selectTag + $selectTagOptions + $optionList + '</select>' );
-				writeToConsole($selectTag, $selectTagOptions, $optionList);
+				
+				// finally make it appear
+				showDropdown();
 			}
 
 			var showDropdown = function(){
+				
 				$('.selectpicker').selectpicker();
-				//$('.selectpicker').hide();
-				// writeToConsole('selectpicker hidden');
-				// $('.selectpicker').show();
-				writeToConsole('selectpicker shown');			
 			}
     
     		// start extension
         	return {
-
 					definition: properties, //returns the contents of properties.js to paint the right side panel for the extension
 
-		            // rendering logic
+					// rendering logic
 		            paint: function ( $element, layout ) {
-
-				       	// writeToConsole($element, layout);
 
 						// Solve the "multiply problem" - Lazy way
 						$element.empty();
@@ -114,13 +116,9 @@ define( [ 	// Note: If you load .js files, omit the file extension, otherwise
 						addBootstrapDiv($element);
 
 						// build and show the dropdown the data
-						buildDropdown('Alpha',0,0,100,1, 'create');
-						showDropdown();
-
+						buildDropdown('Alpha' ,0 ,0 ,maxListEntries ,1 ,'create');
 		           	} //end rendering logic
-
         	}; //end extension
-
 		} // end dependency arguments 
 
 ); // end define
